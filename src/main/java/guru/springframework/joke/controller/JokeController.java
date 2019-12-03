9fc25d9a43dd853801a6ec751b5fc959f3c8d336
tushgee <tushgee@gmail.com>
2019-12-03 12:05:27 -0600

    Initial requirements with custom banner.

---
+++ src/main/java/guru/springframework/joke/controller/JokeController.java
@@ -0,0 +1,24 @@
+package guru.springframework.joke.controller;
+
+import guru.springframework.joke.service.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+
+    }
+}
